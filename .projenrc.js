2bf970130d1670443667d8d164d5dd0221bf0a8b
Matteo Sessa <matteo.sessa@service.vic.gov.au>
2021-09-08 01:04:24 +1000

    enable dom in jest and eslint typescript options

--- .projenrc.js
+++ .projenrc.js
@@ -10,11 +10,17 @@ const project = new TypeScriptAppProject({
   releaseToNpm: true,
   npmAccess: NpmAccess.PUBLIC,
   defaultReleaseBranch: 'main',
+  jest: true,
 
   deps: [
     'cmd-ts@^0.6',
     '@aws-sdk/client-s3',
   ],
 });
+
+// add support for dom library in typescript compiler
 project.tsconfig.file.addOverride('compilerOptions.lib', ['es2018', 'dom']);
+project.tsconfigEslint.file.addOverride('compilerOptions.lib', ['es2018', 'dom']);
+project.tryFindObjectFile('tsconfig.jest.json').addOverride('compilerOptions.lib', ['es2018', 'dom']);
+
 project.synth();
